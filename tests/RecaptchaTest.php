e09eadfaea323b1d0ade556217cb99a9df220996
Vincent Klaiber <vincentklaiber@gmail.com>
2016-11-04 10:07:26 +0100

    Add unit tests

--- tests/RecaptchaTest.php
+++ tests/RecaptchaTest.php
@@ -11,7 +11,13 @@
 
 namespace Vinkla\Tests\Recaptcha;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
+use Vinkla\Recaptcha\Recaptcha;
 
 /**
  * This is the recaptcha test case class.
@@ -20,5 +26,45 @@ use PHPUnit\Framework\TestCase;
  */
 class RecaptchaTest extends TestCase
 {
-    //
+    public function testVerify()
+    {
+        $recaptcha = $this->getRecaptcha(['success' => true]);
+
+        $this->assertTrue($recaptcha->verify('my-recaptcha-response'));
+    }
+
+    /**
+     * @expectedException \Vinkla\Recaptcha\Exceptions\InvalidResponseException
+     */
+    public function testInvalidResponse()
+    {
+        $recaptcha = $this->getRecaptcha(['success' => false]);
+
+        $this->assertTrue($recaptcha->verify('my-recaptcha-response'));
+    }
+
+    /**
+     * @expectedException \Vinkla\Recaptcha\Exceptions\InvalidResponseException
+     */
+    public function testInvalidResponseWithErrorCodes()
+    {
+        $recaptcha = $this->getRecaptcha(['success' => false, 'error-codes' => []]);
+
+        $this->assertTrue($recaptcha->verify('my-recaptcha-response'));
+    }
+
+    protected function getRecaptcha($data)
+    {
+        $stream = Psr7\stream_for(json_encode($data));
+
+        $mock = new MockHandler([
+            new Response(200, [], $stream),
+        ]);
+
+        $handler = HandlerStack::create($mock);
+
+        $client = new Client(['handler' => $handler]);
+
+        return new Recaptcha('my-site-key', 'my-secret-key', $client);
+    }
 }
